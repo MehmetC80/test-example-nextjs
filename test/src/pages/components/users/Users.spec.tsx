f33aa454682eafe9b2c41bff8aed19532e97bf3f
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-05-13 21:43:55 +0200

    add mock rest-api

---
+++ test/src/pages/components/users/Users.spec.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { server } from '../../../../../src/components/mocks/server';
+import Users from '../../../../../src/components/users/Users';
+
+describe('Users', () => {
+  test('renders correctly', () => {
+    render(<Users />);
+    const textElement = screen.getByText('Users');
+    expect(textElement).toBeInTheDocument();
+  });
+
+  it('renders a list of users', async () => {
+    render(<Users />);
+    const users = await screen.findAllByRole('listitem');
+    expect(users).toHaveLength(3);
+  });
+
+  it('renders error', async () => {
+    server.use(
+      rest.get(
+        'https://jsonplaceholder.typicode.com/users',
+        (req, res, ctx) => {
+          return res(ctx.status(500));
+        }
+      )
+    );
+    render(<Users />);
+    const error = await screen.findByText('Error fetching users');
+    expect(error).toBeInTheDocument();
+  });
+});
